e32d2c35cfd8856ba9302eb22e9b787d4c09306f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-07-20 11:31:54 -0400

    Adding initial build

--- flake.nix
+++ flake.nix
@@ -9,12 +9,43 @@
     (let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       pythonPackages = pkgs.python3Packages;
+
+      pycco = pythonPackages.buildPythonPackage rec {
+        name = "pycco";
+        version = "0.6.0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "pycco-docs";
+          repo = "pycco";
+          rev = version;
+          sha256 = "sha256-fo/yb4tAGbbVObnjGxfBYv/2eJ6OIPQCiLZITTMZpzo=";
+        };
+
+        propagatedBuildInputs = [
+          pythonPackages.markdown
+          pythonPackages.pygments
+          pythonPackages.pystache
+          pythonPackages.smartypants
+        ];
+
+        doCheck = false;
+      };
     in {
-      devShell.x86_64-linux =
+      devShell.x86_64-linux.default =
         pkgs.mkShell {
           buildInputs = [
-            pythonPackages.pycco
+            pycco
           ];
         };
+
+      packages.x86_64-linux.default =
+        pkgs.runCommand "website" {
+          src = ./.;
+        } ''
+          cd $src
+          ls -la
+          mkdir -p $out
+          ${pycco}/bin/pycco conda.py --generate_index -d $out
+        '';
     });
 }
