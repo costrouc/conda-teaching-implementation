c2650f9eec28f56e2bd7e9d9ab22b63d504b2675
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-27 13:44:00 +0000

    Adding tarfile member sanitization to extractall()

--- conda.py
+++ conda.py
@@ -416,7 +416,26 @@ def extract_package(directory: pathlib.Path, url: str):
     filename = directory / url.split('/')[-1]
     extract_directory = filename.with_suffix('').with_suffix('')
     with tarfile.open(filename, mode='r:bz2') as tar:
-        tar.extractall(extract_directory)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, extract_directory)
 
 
 def download_packages(directory: str, packages: List[Dict], channel_url: str):
